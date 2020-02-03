16d23ac0aece1b5862a4b39ad3e5a2274381cc71
YL L <18800148409@163.com>
2020-02-03 12:36:56 +0800

    update

--- main.go
+++ main.go
@@ -43,6 +43,7 @@ var (
 	AGENTS                                   map[string]string
 	session_id                               string = ""
 	Host                                     string = ""
+	back string = ""
 	//全局变量
 )
 
@@ -167,82 +168,9 @@ func httpserver(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintln(w, payload)
 		}
 	} else {
-		//全都不匹配输出请求详细
-		//应增加ua头判断
-		//先强制断开连接
-		//fmt.Println(r.Close)
-		////自动关闭服务器
-		//
-		//fmt.Println("Request解析")
-		////HTTP方法
-		//fmt.Println("method", r.Method)
-		//// RequestURI是被客户端发送到服务端的请求的请求行中未修改的请求URI
-		//fmt.Println("RequestURI", r.RequestURI)
-		////URL类型,下方分别列出URL的各成员
-		//fmt.Println("URL_scheme", r.URL.Scheme)
-		//fmt.Println("URL_opaque", r.URL.Opaque)
-		//fmt.Println("URL_user", r.URL.User.String())
-		//fmt.Println("URL_host", r.URL.Host)
-		//fmt.Println("URL_path", r.URL.Path)
-		//fmt.Println("URL_RawQuery", r.URL.RawQuery)
-		//fmt.Println("URL_Fragment", r.URL.Fragment)
-		////协议版本
-		//fmt.Println("proto", r.Proto)
-		//fmt.Println("protomajor", r.ProtoMajor)
-		//fmt.Println("protominor", r.ProtoMinor)
-		//
-		////打印全部头信息
-		//for k, v := range r.Header {
-		//	// fmt.Println("Header key:" + k)
-		//	for _, vv := range v {
-		//		fmt.Println("header key:" + k + "  value:" + vv)
-		//	}
-		//}
-		//
-		////解析body
-		////r.ParseMultipartForm(128)
-		////fmt.Println("解析方式:ParseMultipartForm")
-		//r.ParseForm()
-		//fmt.Println("解析方式:ParseForm")
-		//
-		////body内容长度
-		//fmt.Println("ContentLength", r.ContentLength)
-		//
-		////打印全部内容
-		//fmt.Println("Form", r.Form)
-		//
-		////该请求的来源地址
-		//fmt.Println("RemoteAddr", r.RemoteAddr)
-		//
-		/////data:=r.RemoteAddr
-		////发送邮件通知
-		////SendMail("Danger notice ！！！！",data)
-		////os.Exit(0)
 		fmt.Fprintf(w, "")
 	}
 }
-
-//func SendMail(subject string, body string ) error {
-//    //定义邮箱服务器连接信息
-//    mailConn := map[string]string {
-//        "user": "",
-//        "pass": "",
-//        "host": "",
-//        "port": "",
-//    }
-//
-//    port, _ := strconv.Atoi(mailConn["port"]) //转换端口类型为int
-//
-//    m := gomail.NewMessage()
-//    m.SetHeader("Subject", subject)  //设置邮件主题
-//    m.SetBody("text/html", body)     //设置邮件正文
-//
-//    d := gomail.NewDialer(mailConn["host"], port, mailConn["user"], mailConn["pass"])
-//
-//    err := d.DialAndSend(m)
-//    return err
-//
-//}
 
 //打印全部主机信息
 func info_os() {
@@ -269,7 +197,7 @@ func info_os() {
 		table.Render() // Send output
 	}
 
-} //定义表格  info信息
+}
 
 func Scanf(a *string) {
 	reader := bufio.NewReader(os.Stdin)
@@ -286,11 +214,8 @@ func Scanf(a *string) {
 		//帮助参数
 	} else if string(data) == "back" {
 		*a = ""
-		goto end //选择back 就跳转
-	end:
-		Options()
+		back="back"
 		return
-		//系统退出
 	}
 	*a = string(data)
 
@@ -371,6 +296,10 @@ func Options() { //定义tab 下拉菜单选项参数
 			for true {
 				fmt.Print("Console_shell >")
 				Scanf(&cmd)
+				if back=="back"{
+					back=""
+					break
+				}
 
 			}
 		} else if options == "help" {
